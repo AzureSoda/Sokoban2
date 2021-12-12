41d45d7434d38b3168662e94541a86cbe05a9ac6
AzureSoda <dmb07250@gmail.com>
2021-12-12 18:09:55 +0900

    first

---
+++ sokoban1.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+alist=[]
+blist=[]
+
+stage1=input()
+while True:
+    alist.append(list(map(str,input())))
+    if alist == "=====":
+        break
+
+stage2=input()
+while True:
+    blist.append(list(map(str,input())))
+    if blist== " ":
+        break
+
+""" palist=np.array
+print
+
+#세로 길이 재기
+for 
+
+define rowsize#가로
+print(len(alist))
+print(len(blist))
+define columnsize#세로
+
+define countHall  for i in range if    in a
+
+define countBall
+
+define playerposition
+
+
+
+#map = [[0 for i in range(COLUM)] for j in range(ROW)]  """
