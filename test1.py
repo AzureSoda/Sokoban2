957b82099459ccc7879369bad07216706f80fd81
AzureSoda <dmb07250@gmail.com>
2021-12-13 00:43:49 +0900

    fix2

--- test1.py
+++ test1.py
@@ -1,11 +1,11 @@
-arr=[]
 while True:
+   
     arr = [list(map(str, input().split())) for i in range(4)]
 
-    if arr==3:
-        break
+    for i in arr:        
+        for j in i:   
+            print(j, end=' ')
+        print()
 
-for i in arr:        
-    for j in i:   
-        print(j, end='')
-    print()
+    if arr=='':
+        break
