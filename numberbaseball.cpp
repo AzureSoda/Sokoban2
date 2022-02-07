983651d6defefba45dc3dd389f05ec8f7534274c
김경준 <49617190+AzureSoda@users.noreply.github.com>
2022-02-07 20:18:32 +0900

    Update numberbaseball.cpp

--- numberbaseball.cpp
+++ numberbaseball.cpp
@@ -16,9 +16,10 @@ int main() {
 
 	do {
 		option = 0;
-		std::cout << "계속 할꺼면 1 입력 (0 입력시 종료)";
+		std::cout << "계속 할꺼면 1 입력 (0 입력시 종료)\n";
 		std::cin >> option; // 0 입력시 종료하고 그 외 숫자, 문자 입력시 게임 시작
-		
+		int strike = 0;
+		int ball = 0;
 			srand(time(NULL)); //rand 쓰기전에 초기화
 
 			for (i = 0; i < 9; i++)
@@ -32,43 +33,56 @@ int main() {
 
 			printf("%d\n", comnum);    //// 임시용 숫자확인
 
-			cout << "예상숫자 입력하기";
+			cout << "예상숫자 입력하기\n";
 			cin >> mynum;
 
 			my1 = mynum / 100;
 			my2 = mynum % 100/10;
 			my3 = mynum % 10;
 
-			printf("% d, %d , %d", my1, my2, my3);
+			printf("% d, %d , %d\n", com1, com2, com3);  //임시용 숫자 확인
+			printf("% d, %d , %d\n", my1, my2, my3);
 
-			if (com1 == my1);
-			strike += 1;
-			if (com2 == my2);
-			strike += 1;
-			if (com3 == my3);
-			strike += 1;
+			
+			if (com1 == my1); {
+				strike += 1; 
+				if (com1 == my2 or com1 == my3); {
+				ball += 1; }
+			
+			
+			}
+			
+			
+			if (com2 == my2); {
+				strike += 1;
+				if (com2 == my2 or com2 == my3); {
+					ball += 1; }
+			}
+			
+			
+			
+			if (com3 == my3); {
+				strike += 1;
+				if (com3 == my2 or com3 == my3); {
+					ball += 1; }
+			}
+			
+			
 
-			if (com1 == my2 or com1 == my3);
-			ball += 1;
-
-			if (com2 == my2 or com2 == my3);
-			ball += 1;
-
-			if (com3 == my2 or com3 == my3);
-			ball += 1;
+			
 
 
 			if (ball == 0 && strike == 0)
 				printf("낫싱\n");
 
-			if (ball == 0 && strike != 0)
+			else if (ball == 0 && strike != 0)
 				printf("%d 스트라이크\n", strike);
 
-			if (ball != 0 && strike != 0)
+			else if (ball != 0 && strike != 0)
 				printf("%d 스트라이크, %d 볼\n", strike, ball);
 			
-			if (comnum == mynum)
-				printf("정답입니다");
+			else if (comnum == mynum)
+				printf("정답입니다\n");
 
 
 	} while (option!=0);
